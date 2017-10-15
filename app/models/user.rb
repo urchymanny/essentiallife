8364191274ddb7d8f86630de7b2967b7038f6e4b
urchymanny <urchman0000@gmail.com>
2017-10-15 20:12:58 +0100

    payment

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,9 @@ class User < ApplicationRecord
               :full_name, 
               :phone_number, 
               :address, 
-              :city_id,
+              :city,
+              :state,
+              :country,
               :account_number,
               :bank_name,
               :account_name,  presence: true
@@ -30,67 +32,67 @@ class User < ApplicationRecord
     has_many :bonus_wallet_details
     has_many :wallet_details
 
-    #callbacks
-    before_validation :generate_refferal_id, on: :create
-    before_create :check_for_sponsor, on: :create
+    # #callbacks
+    # before_validation :generate_refferal_id, on: :create
+    # before_create :check_for_sponsor, on: :create
 
-    def assign_user_to_sponsor_downlines(sponsor, user)
-        if sponsor_has_more_than_two_downlines?(sponsor.first_downlines)
-            if sponsor_has_more_than_two_downlines?(sponsor.first_downlines.siblings.first)
-                if sponsor_has_more_than_two_downlines?(sponsor.first_downlines.first_downlines)
-            else
-                user.parent = sponsor.first_downlines.siblings.first
-            end
-        else
-            user.parent = sponsor.first_downlines
-        end
-    end
+    # def assign_user_to_sponsor_downlines(sponsor, user)
+    #     if sponsor_has_more_than_two_downlines?(sponsor.first_downlines)
+    #         if sponsor_has_more_than_two_downlines?(sponsor.first_downlines.siblings.first)
+    #             if sponsor_has_more_than_two_downlines?(sponsor.first_downlines.first_downlines)
+    #         else
+    #             user.parent = sponsor.first_downlines.siblings.first
+    #         end
+    #     else
+    #         user.parent = sponsor.first_downlines
+    #     end
+    # end
    
 
-    def assign_user_to_sponsor(sponsor, user)
-        if sponsor_has_more_than_two_downlines?(sponsor)
-            assign_user_to_sponsor_downlines(sponsor, user)
-        else
-            #add user to sponsor
-            user.parent = sponsor
-        end
-    end
+    # def assign_user_to_sponsor(sponsor, user)
+    #     if sponsor_has_more_than_two_downlines?(sponsor)
+    #         assign_user_to_sponsor_downlines(sponsor, user)
+    #     else
+    #         #add user to sponsor
+    #         user.parent = sponsor
+    #     end
+    # end
 
-    def sponsor_has_more_than_two_downlines?(user)
-        return true if user.children && user.children.count >= 2
-        return false
-    end
+    # def sponsor_has_more_than_two_downlines?(user)
+    #     return true if user.children && user.children.count >= 2
+    #     return false
+    # end
 
 
-    def find_sponsor(user)
-        #find sponsore
-        sponsor = User.find_by(refferal_id: user.sponsor_id)
+    # def find_sponsor(user)
+    #     #find sponsore
+    #     sponsor = User.find_by(refferal_id: user.sponsor_id)
         
-        #assign user to sponsor
-        assign_user_to_sponsor(sponsor, user)
-    end
+    #     #assign user to sponsor
+    #     assign_user_to_sponsor(sponsor, user)
+    # end
 
-    def check_for_sponsor
-        if self.sponsor_id.present?
-            find_sponsor(self)
-        else
-            #assign to default
-            self.sponsor_id = "1000" 
-        end
-    end
+    # def check_for_sponsor
+    #     if self.sponsor_id.present?
+    #         find_sponsor(self)
+    #     else
+    #         #assign to default
+    #         self.sponsor_id = "1000" 
+    #     end
+    # end
 
 
-    def first_downlines
-        self.children.first
-    end
+    # def first_downlines
+    #     self.children.first
+    # end
 
 
     
-    private
+    # private
     
-    def generate_refferal_id
-      begin
-        self.refferal_id = SecureRandom.uuid.gsub("-", "").hex.to_s[1...6]
-      end while self.class.exists?(refferal_id: refferal_id)
-    end
+    # def generate_refferal_id
+    #   begin
+    #     self.refferal_id = SecureRandom.uuid.gsub("-", "").hex.to_s[1...6]
+    #   end while self.class.exists?(refferal_id: refferal_id)
+    # end
 end
