ea12cd5b2dcaf7f631d726dd6cd558cbcc5b4ef4
urchymanny <urchman0000@gmail.com>
2017-10-16 08:58:54 +0100

    Coming Soon

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,6 @@ Rails.application.routes.draw do
 
   resources :sessions
 
-  root "static_pages#register"
+  root "static_pages#coming_soon"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
